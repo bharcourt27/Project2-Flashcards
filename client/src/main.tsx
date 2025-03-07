3ec3a4197519f03928dad1072da3551f4cbfe58f
Brad Harcourt <bradharcourt@Brads-MBP.attlocal.net>
2025-03-06 23:56:33 -0500

    update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,41 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import App from './App.tsx';
-import Board from './pages/Home.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import EditTicket from './pages/EditTicket.tsx';
-import CreateFlashcard from './pages/CreateFlashcard.tsx';
-import Login from './pages/Login.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      }, 
-      {
-        path: '/edit',
-        element: <EditTicket />
-      },
-      {
-        path: '/create',
-        element: <CreateFlashcard />
-      },
-      {
-        path: '/login',
-        element: <Login />
-      }
-    ]
-  }
-])
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
