244917344a57b4a7df06e5469cba81a3a35a0936
“[Quinn <quinnbrown2004@gmail.com>
2025-03-12 18:10:35 -0400

    commit changes

--- client/src/components/Flashcardlist.tsx
+++ client/src/components/Flashcardlist.tsx
@@ -1,16 +1,9 @@
-import Flashcard from './Flashcard'
-import { FlashCardData } from '../interfaces/FlashCardData'
-
-interface flashcardListProps {
-    flashcards: FlashCardData[];
-    removeCard: (id: number) => void;
-}
-export default function flashcardlist({ flashcards,removeCard }: flashcardListProps) {
-  return (
-    <div className= "row">
-        {flashcards?.map((flashcard,i) => {
-            return <Flashcard flashcard={flashcard} removeCard = {removeCard} key={i} />
-        })}
-    </div>
-  )
-}
+export default function flashcardlist({ Flashcards }) {
+    return (
+      <div className= "Card-grid">
+          {Flashcards.map(Flashcard => {
+              return <Flashcard flashcard={Flashcard} key={Flashcard.id} />
+          })}
+      </div>
+    )
+  }
